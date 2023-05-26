43486b457f57aebbb90fa16509a113774c361be4
Randy <1352918721@qq.com>
2023-05-26 23:25:13 +0800

    PlayerController2

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+* 设置人物基础基础速度、加速和体力消耗（加速移动消耗，其他情况回复体力）
+* 加速键设置
+* 
+*/
+public class PlayerController : MonoBehaviour
+{
+    PlayerCharacter character;
+
+    void Start()
+    {
+        
+        character = GetComponent<PlayerCharacter>();
+    }
+
+    // 检测用户是否按下“Z”键以加快角色速度（如果其体力充足）
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && currentStamina > 0)
+        {
+            isSprinting = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Z))
+        {
+            isSprinting = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            character.Jump();
+        }
+    }
+
+    // 处理基于物理引擎的角色运动逻辑，包括控制基础或加强模式速度以及更新剩余体力。
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+         character.Move(moveHorizontal,isSprinting);
+  
+    }
+
+
+}
