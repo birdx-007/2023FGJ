b39ed8bc9baa4110eb6b7f58c218c2948fcc82a7
BirdNoX007 <birdnox007@163.com>
2023-05-26 19:24:59 +0800

    玩家现在的移动模式改为横版移动模式

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,15 @@ using UnityEngine;
 */
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f; // 基础移动速度
+    public float speed = 10f; // 基础移动速度
     public float sprintSpeedMultiplier = 2f; // 加速时的倍数
+    public float jumpForce = 40f;
     public int maxStamina = 100; // 最大体力值
     public int staminaRecoveryRate = 3; // 恢复速率
     public int sprintStaminaCostPerSecond = 5; // 加速消耗
 
     private Rigidbody2D rb;
+    private bool isJumping = false;
     private bool isSprinting = false;
     private int currentStamina;
 
@@ -36,21 +38,27 @@ public class PlayerController : MonoBehaviour
         {
             isSprinting = false;
         }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            Jump();
+        }
     }
 
     // 处理基于物理引擎的角色运动逻辑，包括控制基础或加强模式速度以及更新剩余体力。
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
+        //Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
 
         // 控制基础和加强速度之间的转换
         float currentSpeed = isSprinting ? speed * sprintSpeedMultiplier : speed;
 
         // 控制角色的运动
-        rb.velocity = movement * currentSpeed;
+        //rb.velocity = movement * currentSpeed;
+        rb.velocity = new Vector2(moveHorizontal * currentSpeed, rb.velocity.y);
 
         // 消耗体力（如果加速）或回复体力（如果不加速）
         if (isSprinting)
@@ -61,6 +69,12 @@ public class PlayerController : MonoBehaviour
         {
             RecoverStamina(staminaRecoveryRate * Time.fixedDeltaTime);
         }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isJumping = true;
     }
 
     // 减少当前剩余体力值，同时根据需要停止加速。
@@ -88,6 +102,13 @@ public class PlayerController : MonoBehaviour
             {
                 currentStamina = maxStamina;
             }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
         }
     }
 }
