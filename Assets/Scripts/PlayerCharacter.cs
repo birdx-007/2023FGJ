40f122824b60e9d0f4ce92d1b02139ba34386fe1
BirdNoX007 <birdnox007@163.com>
2023-05-27 09:53:48 +0800

    微调PlayerController 添加属性条预制件

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public PropertyBarController staminaBar;
     //速度
-    public float speed;
-    public float sprintSpeedMultiplier;
+    public float speed = 10f;
+    public float sprintSpeedMultiplier = 2f;
 
-    public float jumpForce;
-    public float maxStamina =100; // 最大体力值
-    public float staminaRecoveryRate ; // 恢复速率
-    public float sprintStaminaCostPerSecond ; // 加速消耗
-
+    public float jumpForce = 40f;
+    public float maxStamina = 100f; // 最大体力值
+    public float staminaRecoveryRate = 20f; // 恢复速率
+    public float sprintStaminaCostPerSecond = 40f; // 加速消耗
 
     private Rigidbody2D rb;
+
     //跳跃
-    private bool isJumping = false;
-    private bool isSprinting = false;
+    public bool isJumping = false;
+    public bool isSprinting = false;
     private float currentStamina;
 
-//攻击
+    //攻击
     private bool attacking = false;
 
-    private int atttackmode ;
+    private int atttackmode;
 
-//生命
-    private float health;
-    public float healthMax;
+    //生命
+    private float currentHealth;
+    public float maxHealth = 15f;
     bool isAlive;
 
 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentStamina = maxStamina;
+        currentHealth = maxHealth;
+    }
 
 
 // 减少当前剩余体力值，同时根据需要停止加速。
@@ -45,6 +53,7 @@ public class PlayerCharacter : MonoBehaviour
             currentStamina = 0;
             isSprinting = false; // 停止加速
         }
+        staminaBar.SetValue(currentStamina/maxStamina);
     }
 
     // 增加当前剩余的体能，如果超过最大值将其去到 maximum。
@@ -59,30 +68,27 @@ public class PlayerCharacter : MonoBehaviour
                 currentStamina = maxStamina;
             }
         }
+        staminaBar.SetValue(currentStamina/maxStamina);
     }
-
-
-public void Move(float moveHorizontal,bool sprinting)// movehorizontal横向，sprinting是否奔跑
-{
-    if(sprinting){
-
-    if (currentStamina > 0)
+    public void Move(float moveHorizontal, bool sprinting) // movehorizontal横向，sprinting是否奔跑
     {
-        isSprinting = true;
-        if (currentStamina <= 0)
+        if (sprinting)
         {
-            currentStamina = 0;
-            isSprinting = false;
+            if (currentStamina > 0)
+            {
+                isSprinting = true;
+                if (currentStamina <= 0)
+                {
+                    currentStamina = 0;
+                    isSprinting = false;
+                }
+            }
+            else
+            {
+                isSprinting = false;
+            }
+
         }
-    }
-    else
-    {
-        isSprinting = false;
-    }
-
-    }
-
-
         // 控制基础和加强速度之间的转换
         float currentSpeed = isSprinting ? speed * sprintSpeedMultiplier : speed;
 
@@ -99,15 +105,14 @@ public void Move(float moveHorizontal,bool sprinting)// movehorizontal横向，s
         {
             RecoverStamina(staminaRecoveryRate * Time.fixedDeltaTime);
         }
-}
-
-
+    }
 
     public void Jump()
     {
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         isJumping = true;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -115,12 +120,4 @@ public void Move(float moveHorizontal,bool sprinting)// movehorizontal横向，s
             isJumping = false;
         }
     }
-
-
-    void Start ()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        currentStamina = maxStamina;
-    }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,41 +10,28 @@ using UnityEngine.UI;
 */
 public class PlayerController : MonoBehaviour
 {
-    public PropertyBarController staminaBar;
-    
-    public float speed = 10f; // 基础移动速度
-    public float sprintSpeedMultiplier = 2f; // 加速时的倍数
-    public float jumpForce = 40f;
-    public float maxStamina = 100f; // 最大体力值
-    public float staminaRecoveryRate = 30; // 恢复速率
-    public float sprintStaminaCostPerSecond = 50; // 加速消耗
-
-    private Rigidbody2D rb;
-    private bool isJumping = false;
-    private bool isSprinting = false;
-    public float currentStamina;
+    PlayerCharacter character;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        currentStamina = maxStamina;
+        character = GetComponent<PlayerCharacter>();
     }
 
     // 检测用户是否按下“Z”键以加快角色速度（如果其体力充足）
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && currentStamina > 0)
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            isSprinting = true;
+            character.isSprinting = true;
         }
         else if (Input.GetKeyUp(KeyCode.Z))
         {
-            isSprinting = false;
+            character.isSprinting = false;
         }
         
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            character.Jump();
         }
     }
 
@@ -52,68 +39,6 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        //float moveVertical = Input.GetAxis("Vertical");
-
-        //Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
-
-        // 控制基础和加强速度之间的转换
-        float currentSpeed = isSprinting ? speed * sprintSpeedMultiplier : speed;
-
-        // 控制角色的运动
-        //rb.velocity = movement * currentSpeed;
-        rb.velocity = new Vector2(moveHorizontal * currentSpeed, rb.velocity.y);
-
-        // 消耗体力（如果加速）或回复体力（如果不加速）
-        if (isSprinting)
-        {
-            ConsumeStamina(sprintStaminaCostPerSecond * Time.fixedDeltaTime);
-        }
-        else
-        {
-            RecoverStamina(staminaRecoveryRate * Time.fixedDeltaTime);
-        }
-    }
-
-    private void Jump()
-    {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        isJumping = true;
-    }
-
-    // 减少当前剩余体力值，同时根据需要停止加速。
-    private void ConsumeStamina(float amount)
-    {
-        if (currentStamina >= amount)
-        {
-            currentStamina -= amount;
-        }
-        else
-        {
-            currentStamina = 0;
-            isSprinting = false; // 停止加速
-        }
-        staminaBar.SetValue(currentStamina/maxStamina);
-    }
-
-    // 增加当前剩余的体能，如果超过最大值将其去到 maximum。
-    private void RecoverStamina(float amount)
-    {
-        if (currentStamina < maxStamina)
-        {
-            currentStamina += amount;
-
-            if (currentStamina > maxStamina)
-            {
-                currentStamina = maxStamina;
-            }
-        }
-        staminaBar.SetValue(currentStamina/maxStamina);
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isJumping = false;
-        }
+        character.Move(moveHorizontal,character.isSprinting);
     }
 }
